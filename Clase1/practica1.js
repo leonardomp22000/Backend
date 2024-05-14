f622fb2c520f128dcc417f3d1d434f45bfc9e880
Leonardo Meza <leonardomp2200@gmail.com>
2024-05-14 09:28:35 -0600

    feat: Practica 1 Reverse

---
+++ Clase1/practica1.js
@@ -0,0 +1,10 @@
+// Poner una cadena en reversa
+
+    const cadena = process.argv[2]
+    let rev = cadena.split('')
+    console.log(rev)
+    let reversa =rev.reverse()
+    console.log(reversa)
+
+
+    
